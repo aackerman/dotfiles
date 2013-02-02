d89778982a6cbcea668cbb2530e4114eb99c8148
Aaron Ackerman <theron17@gmail.com>
2013-02-01 20:54:20 -0600

    Added bootstrap file for autosetup

---
+++ bootstrap.rb
@@ -0,0 +1,46 @@
+require 'fileutils'
+
+def home
+	Dir.home + '/'
+end
+
+def is_mac?
+	RUBY_PLATFORM.downcase.include?("darwin")
+end
+
+def exists_or_link? (file)
+	File.exists?(file) || File.symlink?(file)
+end
+
+# create symlinks to dotfiles in $HOME
+[
+	'.aliases',
+	'.bash_profile',
+	'.profile',
+	'.vimrc',
+	'.bash_prompt'
+].each do | file |
+	homefile = home + file
+	if exists_or_link? homefile
+		FileUtils.rm_f homefile
+	end
+	FileUtils.symlink File.absolute_path(file), homefile
+end
+
+# install fonts
+if is_mac?
+	Dir.glob 'fonts/*.otf' do | file |
+		libraryfile = '/Library/' + file
+		unless exists_or_link? libraryfile
+			FileUtils.cp file, libraryfile
+		end
+	end
+end
+
+unless exists_or_link? home + '.vim'
+	FileUtils.mv '.vim', home + '.vim'
+end
+
+unless exists_or_link? home + 'bin'
+	FileUtils.mv 'bin', home + 'bin'
+end
