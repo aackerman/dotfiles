5927df4ba704b2d536c3334293e74980884a47f8
Aaron Ackerman <theron17@gmail.com>
2014-10-30 22:07:41 -0500

    Progress on bootstrap.go

--- bootstrap.go
+++ bootstrap.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"runtime"
 )
 
@@ -26,11 +27,22 @@ func symlinkDotfiles() {
 
 	dotfiles, err := ioutil.ReadDir("home")
 	if err != nil {
-		log.Println(err)
+		log.Fatalln(err)
 	}
+
 	for _, fi := range dotfiles {
 		if v, ok := osxfiles[fi.Name()]; !ok {
+			repofile := filepath.Abs(fi.Name())
+			homefile := filepath.Join(os.Getenv("HOME"), fi.Name())
 
+			// remove symlink
+			if err := os.Remove(abspath); err != nil {
+				log.Fatalln(err)
+			}
+			// re-symlink
+			if err := os.Symlink(repofile, homefile); err != nil {
+				log.Fatalln(err)
+			}
 		}
 	}
 }
