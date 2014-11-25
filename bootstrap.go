b5027c18f17992d75e1405fd4f5ef8fcd37921fd
Aaron Ackerman <theron17@gmail.com>
2014-11-24 20:09:17 -0600

    Added cp function and finished cp fonts implementation

--- bootstrap.go
+++ bootstrap.go
@@ -12,6 +12,25 @@ func isOSX() bool {
 	return runtime.GOOS == "darwin"
 }
 
+func cp(src, dst string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	// no need to check errors on read only file, we already got everything
+	// we need from the filesystem, so nothing can go wrong now.
+	defer s.Close()
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+	return d.Close()
+}
+
 func fileExists(fname string) bool {
 	if _, err := os.Stat(filename); err == nil {
 		return true
@@ -23,10 +42,16 @@ func installFonts() {
 	if isOSX() {
 		files, err := filepath.Glob("fonts/*.otf")
 		if err != nil {
-			fmt.Fatalf(err)
+			fmt.Fatalln(err)
 		}
 		if files == nil {
-			return
+			fmt.Fatalln("Unable to find fonts")
+		}
+		for _, f := range files {
+			err := cp(f, "/Library/" + f)
+			if err != nil {
+				fmt.Fatalln(err)
+			}
 		}
 	}
 }
